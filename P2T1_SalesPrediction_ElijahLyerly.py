b0a6cf2b307bd4f15d59379dc4ab12b9d40bd2a6
Seidi-Chan <45403778+Seidi-Chan@users.noreply.github.com>
2018-11-29 13:28:35 -0500

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_ElijahLyerly.py
@@ -0,0 +1,10 @@
+# A brief description of the project
+# 09-27-18
+# CTI-110 P2T1 - Sales Prediction
+# Elijah Lyerly
+
+totalsales = float(input('Total sales: '))
+profit = totalsales*0.23 
+print('Annual profit: ', \
+      format(profit, ',.3f'), \
+      sep='')
